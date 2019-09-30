cb45818294031a8cc209272bb3f23398b8865008
peter.franklin <peter.franklin@apexcapitalcorp.com>
2019-09-30 13:41:28 -0500

    Added QuizQuestion

---
+++ src/QuizQuestion.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class QuizQuestion extends Componant {
+  render() {
+    return (
+      <main>
+        <section>
+          <p>// instruction text goes here</p>
+        </section>
+        <section className="buttons">
+          <ul>//quiz question button logic goes here</ul>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default QuizQuestion;
